11c6c12bf42b3b078bec9b0047c95d4d90b6438d
Michael Kumsa <michael.kumsa@astu.edu.et>
2022-12-27 10:03:24 -0800

    initial commit

---
+++ Phoenix Resource Monitor/Phoenix Resource Monitor/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace Phoenix_Resource_Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pRam = new System.Diagnostics.PerformanceCounter();
+            this.pCPU = new System.Diagnostics.PerformanceCounter();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarCPU = new MetroFramework.Controls.MetroProgressBar();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroProgressBarRAM = new MetroFramework.Controls.MetroProgressBar();
+            this.lblCpu = new MetroFramework.Controls.MetroLabel();
+            this.lblRam = new MetroFramework.Controls.MetroLabel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pRam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pRam
+            // 
+            this.pRam.CategoryName = "Memory";
+            this.pRam.CounterName = "% Committed Bytes In Use";
+            // 
+            // pCPU
+            // 
+            this.pCPU.CategoryName = "Processor";
+            this.pCPU.CounterName = "% Processor Time";
+            this.pCPU.InstanceName = "_Total";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.metroLabel1.ForeColor = System.Drawing.Color.White;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 71);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(42, 19);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "CPU: ";
+            this.metroLabel1.UseCustomBackColor = true;
+            this.metroLabel1.UseCustomForeColor = true;
+            // 
+            // metroProgressBarCPU
+            // 
+            this.metroProgressBarCPU.Location = new System.Drawing.Point(71, 67);
+            this.metroProgressBarCPU.Name = "metroProgressBarCPU";
+            this.metroProgressBarCPU.Size = new System.Drawing.Size(290, 23);
+            this.metroProgressBarCPU.TabIndex = 1;
+            this.metroProgressBarCPU.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.metroLabel2.ForeColor = System.Drawing.Color.White;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 104);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(45, 19);
+            this.metroLabel2.TabIndex = 0;
+            this.metroLabel2.Text = "RAM: ";
+            this.metroLabel2.UseCustomBackColor = true;
+            this.metroLabel2.UseCustomForeColor = true;
+            // 
+            // metroProgressBarRAM
+            // 
+            this.metroProgressBarRAM.Location = new System.Drawing.Point(71, 100);
+            this.metroProgressBarRAM.Name = "metroProgressBarRAM";
+            this.metroProgressBarRAM.Size = new System.Drawing.Size(290, 23);
+            this.metroProgressBarRAM.Style = MetroFramework.MetroColorStyle.Purple;
+            this.metroProgressBarRAM.TabIndex = 1;
+            this.metroProgressBarRAM.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // lblCpu
+            // 
+            this.lblCpu.AutoSize = true;
+            this.lblCpu.BackColor = System.Drawing.Color.Transparent;
+            this.lblCpu.ForeColor = System.Drawing.Color.White;
+            this.lblCpu.Location = new System.Drawing.Point(367, 71);
+            this.lblCpu.Name = "lblCpu";
+            this.lblCpu.Size = new System.Drawing.Size(31, 19);
+            this.lblCpu.Style = MetroFramework.MetroColorStyle.Black;
+            this.lblCpu.TabIndex = 0;
+            this.lblCpu.Text = "0 %";
+            this.lblCpu.UseCustomBackColor = true;
+            this.lblCpu.UseCustomForeColor = true;
+            // 
+            // lblRam
+            // 
+            this.lblRam.AutoSize = true;
+            this.lblRam.BackColor = System.Drawing.Color.Transparent;
+            this.lblRam.ForeColor = System.Drawing.Color.White;
+            this.lblRam.Location = new System.Drawing.Point(367, 100);
+            this.lblRam.Name = "lblRam";
+            this.lblRam.Size = new System.Drawing.Size(35, 19);
+            this.lblRam.TabIndex = 0;
+            this.lblRam.Text = "0 % ";
+            this.lblRam.UseCustomBackColor = true;
+            this.lblRam.UseCustomForeColor = true;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea1.Area3DStyle.Enable3D = true;
+            chartArea1.Area3DStyle.Inclination = 20;
+            chartArea1.Area3DStyle.IsClustered = true;
+            chartArea1.Area3DStyle.IsRightAngleAxes = false;
+            chartArea1.Area3DStyle.LightStyle = System.Windows.Forms.DataVisualization.Charting.LightStyle.Realistic;
+            chartArea1.Area3DStyle.Perspective = 1;
+            chartArea1.Area3DStyle.Rotation = 0;
+            chartArea1.Area3DStyle.WallWidth = 15;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.BorderColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.BackColor = System.Drawing.Color.Transparent;
+            legend1.ForeColor = System.Drawing.Color.White;
+            legend1.HeaderSeparatorColor = System.Drawing.Color.White;
+            legend1.ItemColumnSeparatorColor = System.Drawing.Color.White;
+            legend1.Name = "Legend1";
+            legend1.TitleForeColor = System.Drawing.Color.White;
+            legend1.TitleSeparatorColor = System.Drawing.Color.White;
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(17, 126);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.White;
+            series1.LabelForeColor = System.Drawing.Color.White;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            series1.MarkerColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            series1.Name = "CPU";
+            series1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.SmartLabelStyle.CalloutLineColor = System.Drawing.Color.White;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.LabelForeColor = System.Drawing.Color.White;
+            series2.Legend = "Legend1";
+            series2.Name = "RAM";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(385, 113);
+            this.chart1.TabIndex = 2;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Small;
+            this.metroLabel3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.metroLabel3.Location = new System.Drawing.Point(353, 227);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(70, 19);
+            this.metroLabel3.TabIndex = 3;
+            this.metroLabel3.Text = "Version 0.1";
+            this.metroLabel3.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.metroLabel3.UseCustomBackColor = true;
+            this.metroLabel3.UseCustomForeColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 251);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.metroProgressBarRAM);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroProgressBarCPU);
+            this.Controls.Add(this.lblRam);
+            this.Controls.Add(this.lblCpu);
+            this.Controls.Add(this.metroLabel1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Teal;
+            this.Text = "Phoenix Resource Monitor";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pRam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pCPU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.PerformanceCounter pRam;
+        private System.Diagnostics.PerformanceCounter pCPU;
+        private System.Windows.Forms.Timer timer;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarCPU;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBarRAM;
+        private MetroFramework.Controls.MetroLabel lblCpu;
+        private MetroFramework.Controls.MetroLabel lblRam;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+    }
+}
+
---
+++ Phoenix Resource Monitor/Phoenix Resource Monitor/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phoenix_Resource_Monitor
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            float fcpu = pCPU.NextValue();
+            float fram = pRam.NextValue();
+            metroProgressBarCPU.Value = (int)fcpu;
+            metroProgressBarRAM.Value = (int)fram;
+            lblCpu.Text = string.Format("{0:0.0}%", fcpu);
+            lblRam.Text = string.Format("{0:0.0}%", fram);
+            chart1.Series["CPU"].Points.AddY(fcpu);
+            chart1.Series["RAM"].Points.AddY(fram);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        
+    }
+}
